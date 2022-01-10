0e9236ed9e6d8482db01dce366d0fa49d19c10cd
Bhargav Nookala <nooknb@gmail.com>
2022-01-09 16:27:40 -0800

    try different ps script

--- ScanHttpServer/startProcess.ps1
+++ ScanHttpServer/startProcess.ps1
@@ -5,7 +5,23 @@ Start-Transcript -Path runLoopStartup.log
 
 Write-Host Starting Process $ExePath
 while($true){
-    $process = Start-Process $ExePath -PassThru -Wait
+    $pinfo = New-Object System.Diagnostics.ProcessStartInfo
+    $pinfo.FileName = $ExePath
+    $pinfo.RedirectStandardError = $true
+    $pinfo.RedirectStandardOutput = $true
+    $pinfo.UseShellExecute = $false
+    $pinfo.Arguments = "localhost"
+    $process = New-Object System.Diagnostics.Process
+    $process.StartInfo = $pinfo
+    $process.Start() | Out-Null
+    $process.WaitForExit()
+    $stdout = $process.StandardOutput.ReadToEnd()
+    $stderr = $process.StandardError.ReadToEnd()
+    Write-Host "stdout: $stdout"
+    Write-Host "stderr: $stderr"
+    Write-Host "exit code: " + $process.ExitCode
+
+    #$process = Start-Process $ExePath -PassThru -Wait
 
     if($process.ExitCode -ne 0){
         Write-Host Process Exited with errors, please check the logs in $ScanHttpServerFolder\log
